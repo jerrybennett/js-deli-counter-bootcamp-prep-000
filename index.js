77fd13893c0b435e161a0807c92be5c7a053c47f
Jerry Bennett <jerrysbusiness@gmail.com>
2017-09-16 23:18:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function currentLine(line) {
   if( line.length === 0 ) {
     return "The line is currently empty."
   } else {
-    for( var i = 0; i < line.length; i++) {
+    for( var i = 0; i < line.length; i++ ) {
       return "The line is currently: " + line[i] + line[i].value
     }
   }
