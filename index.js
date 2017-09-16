7505786b4a6542a344e44738902a692ba75128de
Jerry Bennett <jerrysbusiness@gmail.com>
2017-09-16 22:53:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function nowServing(katzDeliLine) {
   if( katzDeliLine.length === 0 ) {
     return "There is nobody waiting to be served!"
   } else {
-    katzDeliLine.shift()
-    return `Currently serving ${katzDeliLine[0]}.`
+    return `Currently serving ${katzDeliLine[0]}.` + 
   }
 }
