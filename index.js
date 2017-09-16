e8d46b134eee5e2590f7a500e8df5788b282e7dd
Jerry Bennett <jerrysbusiness@gmail.com>
2017-09-16 23:03:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-function takeANumber( line, name ) {
-  line.push(name)
-  var num = line.length
+function takeANumber( katzDeliLine, name ) {
+  katzDeliLine.push(name)
+  var num = katzDeliLine.length
   return `Welcome, ${name}. You are number ${num} in line.`
 }
 
-function nowServing( line ) {
-  if( line.length === 0 ) {
+function nowServing( katzDeliLine ) {
+  if( katzDeliLine.length === 0 ) {
     return "There is nobody waiting to be served!"
   } else {
-    return `Currently serving ${line.shift(0)}.`
+    return `Currently serving ${katzDeliLine.shift(0)}.`
   }
 }
