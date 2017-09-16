829eab4a2494c027189126f47a3e368d6569322e
Jerry Bennett <jerrysbusiness@gmail.com>
2017-09-16 23:16:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function nowServing( katzDeliLine ) {
 }
 
 function currentLine(line) {
-  if( katzDeliLine.length === 0 ) {
+  if( line.length === 0 ) {
     return "The line is currently empty."
   } else {
     for( var i = 0; i < line.length; i++) {
