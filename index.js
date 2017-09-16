5eb1c934265b977c88e558fb05710641d81b8e50
Jerry Bennett <jerrysbusiness@gmail.com>
2017-09-16 23:26:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function currentLine(line) {
     return "The line is currently empty."
   } else {
     for( var i = 0; i < line.length; i++ ) {
-      return "The line is currently: " + (line.indexOf(i) + 1) + ". " + line[i] + ","
+      return "The line is currently: " + (line.indexOf(i) + 2) + ". " + line[i] + ","
     }
   }
 }
